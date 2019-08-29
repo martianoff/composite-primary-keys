c67049f379eda46e2cb1adbf322943b0a5a730bd
Maksim Martianov <maxmru@yandex.ru>
2019-08-29 14:36:22 -0400

    improved support of sync

--- src/Eloquent/Relationships/CompositeBelongsToMany.php
+++ src/Eloquent/Relationships/CompositeBelongsToMany.php
@@ -51,4 +51,55 @@ class CompositeBelongsToMany extends BelongsToMany
             $value->{$this->relatedKey};
         }, $value) : $value;
     }
+
+    /**
+     * Create a new query builder for the pivot table.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    protected function newPivotQuery()
+    {
+        $query = $this->newPivotStatement();
+
+        foreach ($this->pivotWheres as $arguments) {
+            call_user_func_array([$query, 'where'], $arguments);
+        }
+
+        foreach ($this->pivotWhereIns as $arguments) {
+            call_user_func_array([$query, 'whereIn'], $arguments);
+        }
+
+        return $this->executeWithinOptionalBinaryTransformation(function () use ($query) {
+            return $query->where($this->foreignPivotKey, $this->parent->{$this->parentKey});
+        }, $this->parent);
+    }
+
+    /**
+     * Create a new pivot attachment record.
+     *
+     * @param  int   $id
+     * @param  bool  $timed
+     * @return array
+     */
+    protected function baseAttachRecord($id, $timed)
+    {
+        $record[$this->relatedPivotKey] = $id;
+
+        $record[$this->foreignPivotKey] = $this->executeWithinOptionalBinaryTransformation(function () {
+            return $this->parent->{$this->parentKey};
+        }, $this->parent);
+
+        // If the record needs to have creation and update timestamps, we will make
+        // them by calling the parent model's "freshTimestamp" method which will
+        // provide us with a fresh timestamp in this model's preferred format.
+        if ($timed) {
+            $record = $this->addTimestampsToAttachment($record);
+        }
+
+        foreach ($this->pivotValues as $value) {
+            $record[$value['column']] = $value['value'];
+        }
+
+        return $record;
+    }
 }
